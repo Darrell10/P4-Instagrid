fa0371ade2012f02d57c2c2cf7c7f1718f6c824f
Darrell10 <frederick.port@me.com>
2019-09-07 10:55:58 +0200

    Modification autolayout

--- P4 Instagrid/convertViewToImage.swift
+++ P4 Instagrid/convertViewToImage.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class convertViewToImage: UIImage {
-    static func convert(_ view: UIView, defaultImage: UIImage) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(view.bounds.size, view.isOpaque, 0)
-        defer { UIGraphicsEndImageContext() }
-        view.snapshotView(afterScreenUpdates: true)
-        return UIGraphicsGetImageFromCurrentImageContext() ?? defaultImage
+extension UIView {
+    
+    // Using a function since `var image` might conflict with an existing variable
+    // (like on `UIImageView`)
+    func asImage() -> UIImage {
+        if #available(iOS 10.0, *) {
+            let renderer = UIGraphicsImageRenderer(bounds: bounds)
+            return renderer.image { rendererContext in
+                layer.render(in: rendererContext.cgContext)
+            }
+        } else {
+            UIGraphicsBeginImageContext(self.frame.size)
+            self.layer.render(in:UIGraphicsGetCurrentContext()!)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return UIImage(cgImage: image!.cgImage!)
+        }
     }
 }
